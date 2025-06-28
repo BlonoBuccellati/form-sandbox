629bd101e0fa8bbf711b0e49a5b4fa704fd0d21a
BlonoBuccellati <119178322+BlonoBuccellati@users.noreply.github.com>
2025-06-28 22:40:37 +0900

    説明追加

--- src/app/uncontrolled-default/page.tsx
+++ src/app/uncontrolled-default/page.tsx
@@ -6,7 +6,25 @@ import { Card, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
-const Page = () => {
+const Description = () => {
+  return (
+    <div className='py-10 space-y-4'>
+      <h1 className='text-2xl font-bold'>処理の流れ</h1>
+      <ol className='list-decimal'>
+        <li>Submitイベント発火（あれば）</li>
+        <li>
+          action/methodを見て、actionのURLにページ遷移
+          <br />
+          （actionに指定がなければ同じ画面に遷移するためリロード）
+          <br />
+          ※デフォルトでmethodはgetメソッド
+        </li>
+      </ol>
+    </div>
+  );
+};
+
+const DefaultForm = () => {
   const handlerOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     // submit直後、以下の処理実行後にPOSTされる。
     const form = e.currentTarget; // form要素取得
@@ -17,34 +35,30 @@ const Page = () => {
     // リロード（またはactionのURLに遷移）する。
   };
   return (
+    <Card>
+      <CardTitle>uncontrolled Component form（デフォルト）</CardTitle>
+      <p>古いやり方（使用しない）</p>
+      <p>action: 指定しなければ、ページはリロードされる</p>
+      <form
+        method='post'
+        action='/api/uncontrolled-default'
+        className='space-y-2.5'
+        onSubmit={handlerOnSubmit}
+      >
+        <Label htmlFor='name'>name</Label>
+        <Input type='text' placeholder='name' id='name' name='name' />
+        <Button>送信</Button>
+      </form>
+    </Card>
+  );
+};
+
+const Page = () => {
+  return (
     <div className='p-10'>
-      <div className='py-10 space-y-4'>
-        <h1 className='text-2xl font-bold'>処理の流れ</h1>
-        <ol className='list-decimal'>
-          <li>Submitイベント発火</li>
-          <li>
-            action/methodを見て、actionのURLにページ遷移
-            <br />
-            （actionに指定がなければ同じ画面に遷移するためリロード）
-          </li>
-        </ol>
-      </div>
-      <Card>
-        <CardTitle>uncontrolled Component form（デフォルト）</CardTitle>
-        <p>古いやり方（使用しない）</p>
-        <p>action: 指定しなければ、ページはリロードされる</p>
-        <form
-          method='post'
-          action='/api/uncontrolled-default'
-          className='space-y-2.5'
-          onSubmit={handlerOnSubmit}
-        >
-          <Label htmlFor='name'>name</Label>
-          <Input type='text' placeholder='name' id='name' name='name' />
-          <Button>送信</Button>
-        </form>
-      </Card>
       <ReturnButton />
+      <Description />
+      <DefaultForm />
     </div>
   );
 };
